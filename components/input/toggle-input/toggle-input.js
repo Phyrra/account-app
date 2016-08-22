d0e8e7ea48563b19231cfaa65406918f4acb92cb
Cabadath <none>
2016-08-22 20:03:27 +0200

    added toggle input

---
+++ components/input/toggle-input/toggle-input.js
@@ -0,0 +1,48 @@
+app
+	.component('toggleInput', {
+		templateUrl: 'components/input/toggle-input/toggle-input.html',
+		controller: 'ToggleInputController',
+		controllerAs: 'inputCtrl',
+		bindings: {
+			model: '=ngModel',
+			onValue: '@?',
+			offValue: '@?',
+			disabled: '=ngDisabled'
+		}
+	})
+
+	.controller('ToggleInputController', [function() {
+		var ctrl = this;
+
+		ctrl.values = [];
+		ctrl.state = 0;
+
+		ctrl.onToggle = function() {
+			if (ctrl.disabled) {
+				return;
+			}
+
+			ctrl.state = 1 - ctrl.state;
+			ctrl.model = ctrl.values[ctrl.state];
+
+			console.log('hier', ctrl.state);
+		};
+
+		ctrl.$onInit = function() {
+			if (angular.isUndefined(ctrl.offValue)) {
+				ctrl.offValue = false;
+			}
+
+			if (angular.isUndefined(ctrl.onValue)) {
+				ctrl.onValue = true;
+			}
+
+			ctrl.values = [
+				ctrl.offValue,
+				ctrl.onValue
+			];
+
+			ctrl.state = ctrl.model === ctrl.onValue ? 1 : 0;
+			ctrl.model = ctrl.values[ctrl.state];
+		};
+	}]);
