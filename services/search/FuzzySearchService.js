84c2b91eae6546009ad8ff39aab276602f93dc7d
Cabadath <none@none.com>
2017-05-18 13:58:40 +0200

    improving fuzzy search

--- services/search/FuzzySearchService.js
+++ services/search/FuzzySearchService.js
@@ -1,7 +1,6 @@
 app
 	.service('FuzzySearchService', ['StringMatcher', function(StringMatcher) {
 		var config = {
-			tokenize: true,
 			ignoreCase: true,
 			minLength: 3
 		};
@@ -27,42 +26,84 @@ app
 				});
 		};
 
-		this.getFuzzyResult = function(a, b) {
-			if (config.ignoreCase) {
-				a = a.toLowerCase();
-				b = b.toLowerCase();
-			}
-
-			if (config.tokenize) {
-				var listA = tokenize(a);
-				var listB = tokenize(b);
-
-				var maxMatch = 0;
-				var matches = [];
-
-				for (var i = 0; i < listA.length; ++i) {
-					for (var j = 0; j < listB.length; ++j) {
-						var ai = listA[i];
-						var bj = listB[j];
-
-						var match = StringMatcher.getMatch(ai, bj);
-
-						if (match > maxMatch) {
-							maxMatch = match;
-							matches = [ai, bj];
-						}
+		var joinWithMatch = function(tokens, matchIdx) {
+			return tokens
+				.map(function(token, i) {
+					if (i === matchIdx) {
+						return '<b>' + token + '</b>';
 					}
+
+					return token;
+				})
+				.join(' ');
+		};
+
+		var getSubstringMatch = function(needleTokens, haystack) {
+			var directMatch = null;
+
+			needleTokens.some(function(needle, i) {
+				var idx = haystack.indexOf(needle);
+
+				if (idx !== -1) {
+					directMatch = {
+						match: 1,
+						matches: [
+							joinWithMatch(needleTokens, i),
+							haystack.substring(0, idx) + '<b>' + haystack.substring(idx, idx + needle.length) + '</b>' + haystack.substring(idx + needle.length)
+						]
+					};
+
+					return true;
 				}
 
-				return {
-					match: maxMatch,
-					matches: matches
-				};
-			} else {
-				return {
-					match: StringMatcher.getMatch(a, b),
-					matches: [a, b]
-				};
+				return false;
+			});
+
+			return directMatch;
+		};
+
+		var getClosestWordMatch = function(needleTokens, haystackTokens) {
+			var maxMatch = 0;
+			var maxMatchesIdx = [];
+
+			for (var i = 0; i < needleTokens.length; ++i) {
+				for (var j = 0; j < haystackTokens.length; ++j) {
+					var a = needleTokens[i];
+					var b = haystackTokens[j];
+
+					var match = StringMatcher.getMatch(a, b);
+
+					if (match > maxMatch) {
+						maxMatch = match;
+						maxMatchesIdx = [i, j];
+					}
+				}
 			}
+
+			return {
+				match: maxMatch,
+				matches: [
+					joinWithMatch(needleTokens, maxMatchesIdx[0]),
+					joinWithMatch(haystackTokens, maxMatchesIdx[1])
+				]
+			};
+		};
+
+		this.getFuzzyResult = function(needle, haystack) {
+			if (config.ignoreCase) {
+				needle = needle.toLowerCase();
+				haystack = haystack.toLowerCase();
+			}
+
+			var needleTokens = tokenize(needle);
+
+			var substringMatch = getSubstringMatch(needleTokens, haystack);
+			if (substringMatch) {
+				return substringMatch;
+			}
+
+			var haystackTokens = tokenize(haystack);
+
+			return getClosestWordMatch(needleTokens, haystackTokens);
 		};
 	}]);
